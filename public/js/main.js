0a7f29c15724e92e9bbe0a9a2791eb458ea51135
Vitali Zybaila <vitalizybailo@gmail.com>
2026-08-30 17:27:07 -0400

    fix error js

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
-import './config'
-import './profile'
-import './setting'
-import './calendar'
-import './payments'
-import './quiz'
-import './vocabulary'
-import './premium'
+import './config.js'
+import './profile.js'
+import './setting.js'
+import './calendar.js'
+import './payments.js'
+import './quiz.js'
+import './vocabulary.js'
+import './premium.js'
 
 loadData()
